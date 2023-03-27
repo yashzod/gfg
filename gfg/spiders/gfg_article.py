8dd98ecf4a72a34ed12cd621661f1f4c40cf86dc
yashzod <yashzod748@gmail.com>
2023-03-27 16:47:42 +0530

    modification

--- gfg/spiders/gfg_article.py
+++ gfg/spiders/gfg_article.py
@@ -6,11 +6,19 @@ doc = Document()
 class GfgArticleSpider(scrapy.Spider):
     name = 'gfg_article'
     allowed_domains = ['www.geeksforgeeks.org']
-    start_urls = ['https://www.geeksforgeeks.org/data-structures/?ref=shm']
+    article_url = 'https://www.geeksforgeeks.org/data-structures/?ref=shm'
+    start_urls = [article_url]
 
     def parse(self, response):
         
         title = response.css("h1.entry-title::text").get()
         page_content = response.css('div.page_content *::text').getall()
+
+        doc.add_heading(title)
+        
+        for c in page_content:
+            doc.add_paragraph(c)
+        
+        doc.save('output.docx')
 
 
